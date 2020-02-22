ffe0e1cf1bde23e59d9a9a19e992b364c1eba769
namgungyunhee <yh9189@gmail.com>
2020-02-22 20:41:55 +0900

    add naver login

--- src/main/java/com/yunhee/book/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/yunhee/book/config/auth/dto/OAuthAttributes.java
@@ -25,8 +25,23 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    public static OAuthAttributes of (String registrationId, String userNameAttributes, Map<String, Object> attributes) {
+    public static OAuthAttributes of(String registrationId, String userNameAttributes, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributes, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributes, Map<String, Object> attributes) {
